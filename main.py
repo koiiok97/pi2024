3e80e5426c7e69eac6d848fcce8ae73d0afd6225
koiiok97 <139752206+koiiok97@users.noreply.github.com>
2024-03-29 18:23:33 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# TEMA 2
+
+# Задание 1
+# print(1 != 1)
+
+# Задание 2
+# a, b, c = 1, 2, 3
+# print(a, b, c)
+
+# Задание 3
+# num = int(input("Введите число: "))
+# print(num)
+
+# Задание 4
+# str = "word " * 5
+# print(len(str))
+
+# Задание 5
+# d, m, y = 29, "марта", 2024
+# print(f"Сегодня {d} {m} {y} года", end=". Всего хорошего!\n")
+
+# Задание 6
+# print("Hello" + " my " + "World")
+
+# Задание 7
+# print(len("Hello world"))
+
+# Задание 8
+# print("HELLO WORLD".lower())
+
+# Задание 9
+# n1 = 24
+# n2 = 56
+# n3 = 12
+
+# max_n = max(n1, n2, n3)
+# print(f"Наибольшее число среди {n1}, {n2} и {n3} - {max_n}")
+
+
+# Задание 10
+# s1 = "Hello "
+# s2 = "World"
+# print(f"Объединенная строка - {s1 + s2}")
+
